2a2512801f6d908b753e28f3ef516be6724864be
Lautaro Moyano <lautaromoyanoing@gmail.com>
2024-05-09 20:24:37 -0300

    Ejercicio 10 terminado

--- Ejercicio 10.py
+++ Ejercicio 10.py
@@ -2,19 +2,58 @@
 Juego de Ahorcado: Crea un juego de ahorcado donde el jugador debe adivinar una palabra oculta antes de que se agoten los intentos. 
 Puedes hacerlo con una lista predefinida de palabras."""
 
-import re
 
-def generar_palabra():
-    palabras = [instituto, teclado, tenis, caracol, telefono, procesador, koala, ornitorrinco, perro]
+import random
+
+def seleccionar_palabra():
+    palabras = ['python', 'java', 'javascript', 'html', 'css', 'ruby', 'php', 'cocodrilo']
     return random.choice(palabras)
 
-def pedir_letra():
-    letra = input("Ingrese una letra: ")
-    return letra
+def mostrar_palabra_oculta(palabra, letras_adivinadas):
+    palabra_mostrada = ''
+    for letra in palabra:
+        if letra in letras_adivinadas:
+            palabra_mostrada += letra
+        else:
+            palabra_mostrada += '_'
+    return palabra_mostrada
 
-def juego_ahorcado():
-    palabra = generar_palabra()
-    intentos = 5
+intentos = 6
+letras_adivinadas = []
+palabra_secreta = seleccionar_palabra()
+
+print("¡Bienvenido al Juego del Ahorcado!")
+print("Tienes que adivinar la palabra antes de que se agoten los intentos.")
+print(f"La palabra tiene {len(palabra_secreta)} letras: {mostrar_palabra_oculta(palabra_secreta, letras_adivinadas)}")
+
+while intentos > 0:
+    letra = input('Ingrese una letra: ')
+    letras_adivinadas.append(letra)
+    
+    if letra not in palabra_secreta:
+        print("Letra incorreta!")
+        intentos -= 1
+        print('Te quedan ', intentos, 'intentos.')
+    else:
+        print('Letra Correcta!!')
+
+    palabra_mostrada = mostrar_palabra_oculta(palabra_secreta, letras_adivinadas)
+    print(palabra_mostrada)
+    if '_' not in palabra_mostrada:
+        print("¡Felicidades! ¡Adivinaste la palabra!")
+        break
+
+if intentos == 0:
+    print("NoooooOoOoOo!! Te quedaste intentos. La palabra era:", palabra_secreta)
+
+
+        
+
+    
+
+
+
+
 
     
 

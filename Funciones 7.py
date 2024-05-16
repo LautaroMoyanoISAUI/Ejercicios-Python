a1cfc903e9823901a16f17bcfe6d96309a887409
Lautaro Moyano <lautaromoyanoing@gmail.com>
2024-05-16 00:28:43 -0300

    Correciones

---
+++ Funciones 7.py
@@ -0,0 +1,23 @@
+"""Ejercicio 7
+Contar letras: Crea una función que tome una cadena como entrada y devuelva un
+diccionario con el recuento de cada letra en la cadena."""
+
+def contar_letras(cadena):
+    contador_letras = {}
+    for letra in cadena:
+        if letra != ' ':
+            if letra in contador_letras:
+                contador_letras[letra] += 1
+            else:
+                contador_letras = 1
+    
+    return contador_letras
+
+def principal():
+    cadena = input('Ingrese un texto: ')
+    resultado = contar_letras(cadena)
+
+    print(resultado)
+
+principal()
+
